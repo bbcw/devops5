498e3649845f374c80a742fbd85690684677b778
597004635 <597004635@qq.com>
2017-10-11 13:42:13 +0800

    denghonglin

--- day6/opsweb/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-
-class Profile(models.Model):
-    user    = models.OneToOneField(User,verbose_name="与User模型是一对一关系")
-    name    = models.CharField("中文名",max_length=32)
-    phone   = models.CharField("电话号码",max_length=20)
-    weixin  = models.CharField("weixin_id",max_length=50)
